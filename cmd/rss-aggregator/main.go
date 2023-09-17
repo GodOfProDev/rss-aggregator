3af4b45db3194eeff35068d3d72787f38fc297e5
GodOfPro <1387ilia@gmail.com>
2023-09-17 17:15:58 +0330

    Add chi router

--- cmd/rss-aggregator/main.go
+++ cmd/rss-aggregator/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World")
-
 	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	portString := os.Getenv("PORT")
+
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+
+	err = server.ListenAndServe()
 	if err != nil {
 		panic(err)
 	}
