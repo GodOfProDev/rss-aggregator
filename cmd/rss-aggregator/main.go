46864bc9239d6aaefc4f5945d2c3c86fcc69e6ff
GodOfPro <1387ilia@gmail.com>
2023-09-18 12:25:01 +0330

    Add postgres support (basic)

--- cmd/rss-aggregator/main.go
+++ cmd/rss-aggregator/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	env "github.com/godofprodev/rss-aggregator/internal/config"
 	"github.com/godofprodev/rss-aggregator/internal/router"
+	_ "github.com/lib/pq"
 )
 
 func main() {
