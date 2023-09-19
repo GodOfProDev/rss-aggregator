e9456d45f7bd1ccf90684953c49675124b4bfe86
GodOfPro <1387ilia@gmail.com>
2023-09-19 17:58:13 +0330

    Fix post handler

--- internal/config/api.go
+++ internal/config/api.go
@@ -1,10 +1,14 @@
 package config
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/godofprodev/rss-aggregator/internal/database"
 	"github.com/godofprodev/rss-aggregator/internal/utils"
+	"github.com/google/uuid"
 	"net/http"
 	"sync"
+	"time"
 )
 
 type ApiConfig struct {
@@ -24,8 +28,29 @@ func GetApiConfig() *ApiConfig {
 	return apiInstance
 }
 
-func (api *ApiConfig) HandleCreateUser(w http.ResponseWriter, r *http.Request) {
-	utils.RespondWithJSON(w, http.StatusOK, struct {
-		Username string `json:"username"`
-	}{Username: "GodOfPro"})
+func (apiCfg *ApiConfig) HandleCreateUser(w http.ResponseWriter, r *http.Request) {
+
+	type parameters struct {
+		Name string `json:"name"`
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		utils.RespondWithError(w, 400, fmt.Sprintf("Error parsing JSON:", err))
+		return
+	}
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+	if err != nil {
+		utils.RespondWithError(w, 400, fmt.Sprintf("Couldn't create user:", err))
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, user)
 }
